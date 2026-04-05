678035b13b0e5db5d667bd6cd7097cc1eceb0e14
noyu <devnoyu@gmail.com>
2026-04-04 20:41:38 -0400

    Added: comments on firebase race usehook

---
+++ src/hooks/use-firestore-races.ts
@@ -0,0 +1,18 @@
+
+/*
+    TODO
+
+    Get Races this year
+    Format the data in a useStates
+        Race Entity Diaagram for firestore
+        race_num:             Int64
+        teams:                <Array<String>>
+        scheduled_timestampe: <timestamp>
+        completed_time_ms:    <Array<Int64>>
+
+        completed_time_ms is in the same order as team i.e. 
+        teams: ["team1", "team2"]
+        completed_time_ms: [150000, 138000]
+        means: team1 completed the race in 150000 milliseconds, team2 in 1380000 milliseconds
+
+*/
